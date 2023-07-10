8a3526d64316f8902d348bc2c834cc5a0b61b3d1
rasel-helios <rasel.hossain@heliosinsider.com>
2023-07-10 16:43:12 +0600

    [Done] redux recap runing

---
+++ src/redux/features/cart/cartSlice.ts
@@ -0,0 +1,19 @@
+// import { IProduct } from "@/types/globalTypes";
+// import { createSlice } from "@reduxjs/toolkit";
+
+// interface ICart {
+//     products:IProduct[];
+// }
+
+// const initialState:ICart = {
+//     products:[],
+// }
+
+// const cartSlice = createSlice({
+//     name:"cart"
+//     initialState:{
+//         products:[]
+//     },
+//     reducers:{}
+
+// })
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./features/counter/counterSlice";
-import logger from "./middleware/logger";
+import { configureStore } from '@reduxjs/toolkit';
 
 const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  reducer: {},
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+
 export type AppDispatch = typeof store.dispatch;
 
 export default store;
